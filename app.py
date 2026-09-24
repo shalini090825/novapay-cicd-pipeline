ad968a12c54bcd2f9e005ce41be6eb453d9d5939
shalini090825 <shaald08@gmail.com>
2026-09-24 12:45:10 +0530

    Create app.py
    create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>NovaPay Digital Bank</h1>
+    <p>Application is running successfully!</p>
+    <p>Version: 1.0</p>
+    """
+
+@app.route("/health")
+def health():
+    return "Healthy"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
